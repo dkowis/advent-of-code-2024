4403e36fbef71ddde6e775357c587ca7d3783701
David Kowis <david@kow.is>
2024-12-02 18:42:11 -0600

    Did some restructuring, now it's time for HELLA restructuring

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -18,45 +18,77 @@ fn main() -> Result<(), DayError> {
         .collect();
     let part1_result = part1(&matrix)?;
     println!("Part 1: {}", part1_result);
+    assert_eq!(part1_result, 472);
 
     Ok(())
 }
 
 fn part1(matrix: &Vec<Vec<usize>>) -> Result<usize, DayError> {
-    let transformed: Vec<Vec<isize>> = matrix
-        .iter()
-        .map(|row| {
-            debug!("row: {:#?}", row);
-            let mut result_vec: Vec<isize> = Vec::with_capacity(row.len() - 1);
-            debug!("result_vec: {:#?}", result_vec);
-            for i in 0..row.len() - 1 {
-                result_vec.push(row[i] as isize - row[i + 1] as isize);
-            }
-            result_vec
-        })
-        .collect();
+    let transformed = transform_reports(matrix);
     debug!("transformed: {:#?}", transformed);
     //if any of the changes in levels is more than 3 it's a fail
 
-    let count = transformed
-        .iter()
-        .filter(|row| {
-            row.iter().all(|x| x.abs() > 0 && x.abs() <= 3)
-                && (row.iter().all(|x| x.is_negative()) || row.iter().all(|x| x.is_positive()))
-        })
-        .count();
+    let count = transformed.iter().filter(|report| report.is_safe() ).count();
 
     Ok(count)
 }
 
-fn part2() -> Result<(), DayError> {
-    todo!();
+#[derive(Debug)]
+struct Report {
+    row: Vec<usize>,
+    transformed: Vec<isize>,
+}
+
+impl Report {
+    fn transform_report(row: &Vec<usize>) -> Vec<isize> {
+        let mut result_vec: Vec<isize> = Vec::with_capacity(row.len() - 1);
+        debug!("result_vec: {:#?}", result_vec);
+        for i in 0..row.len() - 1 {
+            result_vec.push(row[i] as isize - row[i + 1] as isize);
+        }
+        result_vec
+    }
+    pub fn new(row: Vec<usize>) -> Report {
+        let transformed = Report::transform_report(&row);
+        Report { row, transformed }
+    }
+    pub fn is_safe(&self) -> bool {
+        self.transformed.iter().all(|x| x.abs() > 0 && x.abs() <= 3)
+            && (self.transformed.iter().all(|x| x.is_negative())
+                || self.transformed.iter().all(|x| x.is_positive()))
+    }
+}
+
+fn transform_reports(matrix: &Vec<Vec<usize>>) -> Vec<Report> {
+    let transformed: Vec<Report> = matrix
+        .iter()
+        .map(|row| {
+            debug!("row: {:#?}", row);
+            Report::new(row.clone())
+        })
+        .collect();
+    transformed
+}
+
+
+fn dampener(row: &Vec<usize>) -> Result<Vec<isize>, DayError> {
+    //return only the fixed one, or none if it cannot be fixed.
+    //somehow, I need to figure out if any one problem would break things, does it fix things?
+    //find any item in the array that doesn't meet the requirements, remove it and try again
+
+    todo!()
+}
+
+fn part2(matrix: &Vec<Vec<usize>>) -> Result<usize, DayError> {
+    let transformed = transform_reports(matrix);
+
+    todo!()
 }
 
 #[cfg(test)]
 mod test {
     extern crate indoc;
-    use crate::part1;
+    use crate::{part1, part2};
     use pretty_assertions::{assert_eq, assert_ne};
     use shared::prelude::*;
     use std::sync::Once;
@@ -93,9 +125,8 @@ mod test {
 
         debug!("input: {:?}", input);
         let result = part1(&input)?;
+        assert_eq!(result, 2);
 
-        assert_eq!(1, 1);
-        assert_ne!(1, 2);
         Ok(())
     }
 
@@ -103,8 +134,24 @@ mod test {
     fn day2_part_two() -> Result<(), DayError> {
         initialize();
 
-        assert_eq!(1, 1);
-        assert_ne!(1, 2);
+        let input_string = r#"
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+        "#
+        .trim();
+
+        let input: Vec<Vec<usize>> = input_string
+            .split("\n")
+            .map(|y| y.split(" ").map(|a| a.parse().unwrap()).collect())
+            .collect();
+
+        debug!("input: {:?}", input);
+        let result = part2(&input)?;
+        assert_eq!(result, 4);
         Ok(())
     }
 }
